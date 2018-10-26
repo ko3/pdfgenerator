69016ee4802c3aa3b02b6c449900745fa11083de
ko3 <ek.lft15@gmail.com>
2018-10-26 17:00:28 -0500

    @Intial setup

---
+++ src/test/java/com/monks/innovation/pdfgenerator/PdfgeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monks.innovation.pdfgenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdfgeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
